43f6f8f3177d01e69bb8e56da11a5165aebb01ec
Waaflee <agustinwafle@gmail.com>
2018-04-28 18:26:27 -0300

    Ejercicio 1 resuelto

---
+++ GPIO/ejercicio_1/main.c
@@ -0,0 +1,18 @@
+// #define POLOLU
+#define NUM_STEPPERS 1
+
+#include "./lib/AVRDuino/A4988.h"
+#include "./lib/AVRDuino/A4988_interrups.h"
+#include "./lib/AVRDuino/core.h"
+#include <util/delay.h>
+#define FALSE 0
+#define TRUE 1
+
+int main(void) {
+  setPin(13, OUTPUT);
+  while (1) {
+    /* code */
+    togglePin(13);
+    _delay_ms(500);
+  }
+}
